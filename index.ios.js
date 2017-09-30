952777afdb545f27670400ea2ffb3681f6a63d94
Brianna <brianna@Briannas-MacBook-Air.local>
2017-09-29 23:26:40 -0700

    first commit

--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,7 @@ export default class CounterApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          COUNTER!
         </Text>
       </View>
     );
@@ -37,17 +30,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('CounterApp', () => CounterApp);
